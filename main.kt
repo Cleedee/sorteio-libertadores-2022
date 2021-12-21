e724d62fae1b44e1615aade6d03b33401f5e42fe
Claudio Torcato <claudiotorcato@gmail.com>
2021-12-21 11:57:08 -0300

    listando os times do pote 1

--- main.kt
+++ main.kt
@@ -1,6 +1,12 @@
+
+
 class Time(val nome: String, val pais: String)
 
-var pote1 = multableListOf(
+var pote1 = mutableListOf(
     Time("Palmeiras", "BRA"),
     Time("Riverplate", "ARG")
 )
+
+fun main() {
+    println(pote1)
+}
