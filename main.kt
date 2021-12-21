bfe44ab7cb63961b90140c504ae1be5ea70f5149
Claudio Torcato <claudiotorcato@gmail.com>
2021-12-21 12:01:32 -0300

    todos os times do pote 1

--- main.kt
+++ main.kt
@@ -2,9 +2,16 @@
 
 class Time(val nome: String, val pais: String)
 
+// declarando os potes
 var pote1 = mutableListOf(
     Time("Palmeiras", "BRA"),
-    Time("Riverplate", "ARG")
+    Time("River Plate", "ARG"),
+    Time("Boca Jrs", "ARG"),
+    Time("Flamengo", "BRA"),
+    Time("Nacional", "URU"),
+    Time("Peñarol", "URU"),
+    Time("Athlético", "BRA"),
+    Time("Atlético-MG", "BRA")
 )
 
 fun main() {
